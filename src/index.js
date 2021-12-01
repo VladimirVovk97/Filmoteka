a1b2fb9e2b76b92d77752d46483e56a5a829ade5
Kruchyna Elena <kruchynaelena@gmail.com>
2021-12-01 02:09:32 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 //main
 
 //footer
-import './.partials/team-list.json';
+//import './.partials/team-list.json';
+
 import './.partials/js/modal-team-list';
 import './.partials/js/markup-films';
 import './.partials/js/modal-film-description';
